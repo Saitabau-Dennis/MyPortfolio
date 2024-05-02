5b401dd3ef4a8aa226efbb302176725613d1e3ea
Saitabau-Dennis <dennisntete28@gmail.com>
2024-05-02 12:27:46 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.html"],
+  theme: {
+    extend: {
+      backgroundColor:{
+        'cherry-red':'#990011',
+      }
+    },
+  },
+  plugins: [],
+}
+
